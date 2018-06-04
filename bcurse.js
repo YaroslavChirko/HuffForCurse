2e118a85448ca3b9fa11330c2f2df65cd0cc48fb
YaroslavChirko <31710480+YaroslavChirko@users.noreply.github.com>
2018-06-04 09:49:34 +0300

    Update bcurse.js

--- bcurse.js
+++ bcurse.js
@@ -1,14 +1,38 @@
 'use strict';
 const str = 'Leptinella filiformis, or slender button daisy';
-const arry = [];
-let mark = 0;
+const treecontainer = [];
+
+class Tree {
+  constructor(name, entrynum) {
+    this.name  = name;
+    this.entrynum = entrynum;
+    this.altnam  = null;
+    this.right = null;
+    this.left = null;
+  }
+  merge(onenode, othernode) {
+    this.name  = onenode.name + othernode.name;
+    this.entrynum = onenode.entrynum + othernode.entrynum;
+    this.right = onenode;
+    this.left = othernode;
+  }
+  huffname(node, string) {
+    if (node) {
+      this.huffname(node.left, string + '0');
+      if (node.left === null && node.right === null) {
+        node.altnam = string;
+      }
+      this.huffname(node.right, string + '1');
+    }
+  }
+}
 
 function bubbleSort(a) {
   let swapped;
   do {
     swapped = false;
     for (let i = 0; i < a.length - 1; i++) {
-      if (a[i].same > a[i + 1].same) {
+      if (a[i].entrynum > a[i + 1].entrynum) {
         const temp = a[i];
         a[i] = a[i + 1];
         a[i + 1] = temp;
@@ -19,45 +43,19 @@ function bubbleSort(a) {
   while (swapped);
 }
 
-function newnam(nod, el, ar) {
-  if (nod) {
-    newnam(nod.left, el, ar);
-    if (nod.name === el) {
-      ar.push(nod.altnam);
+function newname(node, char, ar) {
+  if (node) {
+    newname(node.left, char, ar);
+    if (node.name === char) {
+      ar.push(node.altnam);
       ar.shift();
     }
-    newnam(nod.right, el, ar);
+    newname(node.right, char, ar);
   }
 }
 
-class Tree {
-  constructor(name, same) {
-    this.name  = name;
-    this.same = same;
-    this.altnam  = null;
-    this.right = null;
-    this.left = null;
-  }
-  merg(one, otr) {
-    this.name  = one.name + otr.name;
-    this.same = one.same + otr.same;
-    this.right = one;
-    this.left = otr;
-  }
-  alter(nod, str) {
-    if (nod) {
-      this.alter(nod.left, str + '0');
-      if (nod.left === null && nod.right === null) {
-        nod.altnam = str;
-      }
-      this.alter(nod.right, str + '1');
-    }
-  }
-}
-
-function tree(line, arrr) {
-  const arr1 = line.split('');
-  const arr = line.split('');
+function uniqueCharacter(string) {
+  const arr = string.split('');
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (arr[i] === arr[j]  && i !== j) {
@@ -66,80 +64,99 @@ function tree(line, arrr) {
     }
   }
   const narr  = arr.filter((el) => el !== undefined);
-  for (let i = 0; i < narr.length; i++) {
+  return narr;
+}
+
+function creatingNodes(ucarr, initialarr, nodearr) {
+  for (let i = 0; i < ucarr.length; i++) {
     let count = 0;
-    for (let j = 0; j < arr.length; j++) {
-      if (narr[i] === arr1[j]) {
+    for (let j = 0; j < initialarr.length; j++) {
+      if (ucarr[i] === initialarr[j]) {
         count++;
       }
     }
-    const tmp = new Tree(narr[i], count);
-    arrr.push(tmp);
+    const tmp = new Tree(ucarr[i], count);
+    nodearr.push(tmp);
   }
-  bubbleSort(arrr);
+  bubbleSort(nodearr);
+  return nodearr;
+}
+
+function nodesToTree(nodes) {
   const i = 0;
-  while (i < arrr.length - 1) {
-    if (arrr.length > 2 && arrr[i].same > arrr[i + 2].same) {
+  while (i < nodes.length - 1) {
+    if (nodes.length > 2 && nodes[i].entrynum > nodes[i + 2].entrynum) {
       const tmp = new Tree('nam', 0);
-      tmp.merg(arrr[i + 1], arrr[i + 2]);
+      tmp.merge(nodes[i + 1], nodes[i + 2]);
       const tempo = new Tree('num', 0);
-      tempo.merg(arrr[i], tmp);
+      tempo.merge(nodes[i], tmp);
       let n = 0;
       while (n < 3) {
-        arrr.shift();
+        nodes.shift();
         n++;
       }
-      arrr.unshift(tempo);
+      nodes.unshift(tempo);
     } else {
       const tmp = new Tree('nam', 0);
-      tmp.merg(arrr[i], arrr[i + 1]);
+      tmp.merge(nodes[i], nodes[i + 1]);
       let n = 0;
       while (n < 2) {
-        arrr.shift();
+        nodes.shift();
         n++;
       }
-      arrr.unshift(tmp);
+      nodes.unshift(tmp);
     }
   }
-  arrr[0].alter(arrr[0], '');
-  for (let i = 0; i < arr1.length; i++) {
-    newnam(arrr[0], arr1[0], arr1);
+}
+
+function treeToCharArr(treearr, initarr) {
+  treearr[0].huffname(treearr[0], '');
+  for (let i = 0; i < initarr.length; i++) {
+    newname(treearr[0], initarr[0], initarr);
   }
-  const hufstr = arr1.join('');
+}
+
+function lineToBinary(line, containerarray) {
+  const uniquechararr = uniqueCharacter(line);
+  const linearr = line.split('');
+  creatingNodes(uniquechararr, linearr, containerarray);
+  nodesToTree(containerarray);
+  treeToCharArr(containerarray, linearr);
+  const hufstr = linearr.join('');
   return hufstr;
 }
 
-function untree(str, tr) {
-  const arr  = str.split('');
-  const lengt = arr.length;
-  search(arr, tr, 0, lengt);
-  return arr.join('');
+function binaryToLine(str, hufftree) {
+  const stringarr  = str.split('');
+  const lengt = stringarr.length;
+  search(stringarr, hufftree, 0, lengt, 0);
+  return stringarr.join('');
 }
 
-function search(arr, tree, cnt, ln) {
-  let nod = tree;
-  while (nod.left !== null && nod.right !== null) {
-    if (arr[cnt] === '0') {
-      nod = nod.left;
-      cnt++;
+function search(array, tree, index, size, mark) {
+  let treeel = tree;
+  while (treeel.left !== null && treeel.right !== null) {
+    if (array[index] === '0') {
+      treeel = treeel.left;
+      index++;
     } else {
-      nod = nod.right;
-      cnt++;
+      treeel = treeel.right;
+      index++;
     }
   }
-  for (let i = 0; i < cnt; i++) {
-    arr.shift();
+  for (let i = 0; i < index; i++) {
+    array.shift();
     mark++;
   }
-  arr.push(nod.name);
-  if (mark === ln) {
+  array.push(treeel.name);
+  if (mark === size) {
     return;
   } else {
-    search(arr, tree, 0, ln);
+    search(array, tree, 0, size, mark);
   }
 }
 
-const lin = tree(str, arry);
-const bck = untree(lin, arry[0]);
+const bin = lineToBinary(str, treecontainer);
+const lin = binaryToLine(bin, treecontainer[0]);
+console.log(bin);
 console.log(lin);
-console.log(bck);
